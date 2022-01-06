091bbee79910dff5a20d9350f8aa0cfd64c36f75
rose17-creator <58398483+rose17-creator@users.noreply.github.com>
2022-01-07 01:43:02 +0530

    Delete ytdata.py

--- ytdata.py
+++
@@ -1,8 +0,0 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-def get_ytdata(id):
-    transcript_list = YouTubeTranscriptApi.get_transcript(id)
-    full_transcript = ""
-    for transcript in transcript_list:
-        timestamp = transcript['text'].split()
-        full_transcript+=" "+" ".join(timestamp)
-    return full_transcript
